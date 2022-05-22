600a523b7ee581fe04e642f8f082529bb8978608
clock-en <k.kuroda.biz@gmail.com>
2022-05-22 13:03:13 +0000

    Initial Commit

---
+++ .devcontainer/images/mysql/initdb.d/02_data.sql
@@ -0,0 +1,7 @@
+USE map_app;
+
+INSERT INTO users (name, email, password) VALUES (
+    'test1',
+    'test1@example.com',
+    '2bbe0c48b91a7d1b8a6753a8b9cbe1db16b84379f3f91fe115621284df7a48f1cd71e9beb90ea614c7bd924250aa9e446a866725e685a65df5d139a5cd180dc9'
+);
---
+++ app/test.py
@@ -0,0 +1,2 @@
+import hashlib
+print(hashlib.sha512('test1234'.encode("utf-8")).hexdigest())
